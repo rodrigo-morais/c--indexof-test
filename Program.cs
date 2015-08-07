5f9bf0c155b378996bc0b5ee1f9687906a1e18dd
Rodrigo Morais <morais.rm@gmail.com>
2015-08-07 09:56:59 -0300

    Solve the problem.

--- Program.cs
+++ Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace run_indexof
 {
-    class Program
+    public class Run
     {
-        static void Main(string[] args)
+        public static int IndexOfLongestRun(string str)
         {
+            var length = 0;
+            var run = "";
+            var firstPosition = 0;
+            var position = 0;
+
+            for (var counter = 0; counter < str.Length; counter = counter + 1)
+            {
+                if (run.Contains(str.Substring(counter, 1)))
+                {
+                    run = run + str.Substring(counter, 1);
+                }
+                else
+                {
+                    if (run.Length > 0 && run.Length >= length)
+                    {
+                        length = run.Length;
+                        position = firstPosition;
+                    }
+                    run = str.Substring(counter, 1);
+                    firstPosition = counter;
+                }
+            }
+
+            if (run.Length > 0 && run.Length >= length)
+            {
+                length = run.Length;
+                position = firstPosition;
+            }
+
+            return position;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IndexOfLongestRun("abbcccddddcccbba"));
+            
+
+            Console.WriteLine(IndexOfLongestRun("abcdefg"));
+            Console.Read();
         }
     }
 }
